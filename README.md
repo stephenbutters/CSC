a791293d353d9600bde435cda22653e0d5d8bb9e
Hongkan Liu <hongkanliu@gmail.com>
2017-10-14 11:36:23 -0700

    dynamic page load update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# CS130Project
-This is the project in CS 130 Fall 2017
+This is the project for CS 130 in Fall 2017
+
+Dynamicly load contents into pages, no need to hardcode
+
+But need files containing all those subject names and class names
+
+Then we can use PHP to get all those into arrays in my-app.js
--- js/my-app.js
+++ js/my-app.js
@@ -47,7 +47,7 @@ myApp.onPageInit('about', function (page) {
         createContentPage();
     });
 });
-
+var firstLogin = 1;
 $$('.login-screen .list-button').on('click', function () {
     var uname = $$('.login-screen input[name = "username"]').val();
     var pwd = $$('.login-screen input[name = "password"]').val();
@@ -62,16 +62,69 @@ $$('.login-screen .list-button').on('click', function () {
             myApp.closeModal('.login-screen');
             mainView.router.back();
             $$('.login-screen input[name = "password"]').val("");
+            if(firstLogin) {
+                addSubjects();
+                firstLogin = 0;
+            }
         }, 1500);
     } else {
         myApp.alert('One of your username and password is wrong.', 'ERROR');
     }
  });
 
+var inClassPage = 0;
+var classPageName;
+var classNum = -1;
+setInterval(function() {
+    if(mainView.activePage.name == "index" && inClassPage) {
+        inClassPage = 0;
+        var allPages = $$('#allPages');
+        var page = document.createElement("div");
+        page.className = ("page cached");
+        page.setAttribute("data-page", classPageName);
+        var bDom = '<form data-search-list=".list-search' + classPageName + '" data-search-in=".item-title"' +
+                'class="searchbar searchbar-init" style="top: 65px; background-color: #4a5965; width: 90%; left: 5%; border-radius: 12px;"> <div class="searchbar-input">' +
+                '<input type="search" style="border-radius: 12px" placeholder="Search Class">' +
+                '<a href="#" class="searchbar-clear"></a></div></form> <div class="searchbar-overlay">' +
+                '</div> <div class="page-content" style="top: 55px; text-align: center;">' +
+                '<div class="content-block"> <div class="list-block list-search' + classPageName + ' searchbar-found">' +
+                '<ul class="ul' + classPageName + '"></ul></div></div></div></div>';
+        page.innerHTML = bDom;
+        allPages.append(page);
+        var nb = document.createElement("div");
+        nb.className = "navbar-inner cached";
+        nb.setAttribute("data-page", classPageName);
+        var cDom = '<div class="left"><a href="#" class="back link"> <i class="icon icon-back">' +
+                '</i><span>Back</span></a></div><div class="right" style="margin-right: 20px;">' +
+                '<a href="#" class="link icon-only open-panel"></i></a></div></div>';
+        nb.innerHTML = cDom;        
+        navbars.append(nb);
+        for(var j = 0; j < subjectClasses[classNum].length; j++) {
+            var lidom = document.createElement("li");
+            lidom.className = "item-content"; 
+            var dDom = '<a href="subject'+classNum+String.fromCharCode(65+classNum) +
+                        '" class="item-link"> <div class="item-content"> <div class="item-inner">' +
+                        '<div class="item-title">' + subjectClasses[classNum][j] + '</div></div></div></a>';
+            var ulname = ".ulsubject" + classNum;
+            var ul = $$(ulname);
+            lidom.innerHTML = dDom;
+            ul.append(lidom);
+        }
+    }
+    for(var i = 0; i < subjects.length; i++) {
+        if(mainView.activePage.name == ("subject"+i)) {
+            inClassPage = 1;
+            classNum = i;
+            classPageName = "subject" + i;
+        }
+    }
+}, 500);
+
 $$('#backToIndexBtn').on('click', function() {
     mainView.router.back();
 });
 
+setInterval(function(){ console.log(mainView.activePage.name); }, 100);
 
 // Generate dynamic page
 var dynamicPageIndex = 0;
@@ -100,4 +153,57 @@ function createContentPage() {
         '</div>'
     );
 	return;
+}
+
+var subjects = ["test1", "test2", "test3"];
+var subjectClasses = [["class0", "class1", "class2"],
+                        ["class3", "class4", "class5"],
+                        ["class6", "class7", "class8"]];
+
+function addSubjects() {
+    var subjectul = $$('#subjectUL');
+    for(var i = 0; i < subjects.length; i++) {
+        var lidom = document.createElement("li");
+        lidom.className = "item-content";
+        var aDom = '<a href="#subject' + i + '" class="item-link">' +
+                    '<div class="item-content"> <div class="item-inner">' +
+                    '<div class="item-title">' + subjects[i] + '</div></div></div></a>';
+        lidom.innerHTML = aDom;
+        subjectul.append(lidom);
+    }
+    var allPages = $$('#allPages');
+    var navbars = $$('#navbars');
+    for(var i = 0; i < subjects.length; i++) {
+        var page = document.createElement("div");
+        page.className = ("page cached");
+        page.setAttribute("data-page", "subject"+i);
+        var bDom = '<form data-search-list=".list-searchsubject' + i + '" data-search-in=".item-title"' +
+                'class="searchbar searchbar-init" style="top: 65px; background-color: #4a5965; width: 90%; left: 5%; border-radius: 12px;"> <div class="searchbar-input">' +
+                '<input type="search" style="border-radius: 12px" placeholder="Search Class">' +
+                '<a href="#" class="searchbar-clear"></a></div></form> <div class="searchbar-overlay">' +
+                '</div> <div class="page-content" style="top: 55px; text-align: center;">' +
+                '<div class="content-block"> <div class="list-block list-searchsubject' + i + ' searchbar-found">' +
+                '<ul class="ulsubject' + i + '"></ul></div></div></div></div>';
+        page.innerHTML = bDom;
+        allPages.append(page);
+        var nb = document.createElement("div");
+        nb.className = "navbar-inner cached";
+        nb.setAttribute("data-page", "subject"+i);
+        var cDom = '<div class="left"><a href="#" class="back link"> <i class="icon icon-back">' +
+                '</i><span>Back</span></a></div><div class="right" style="margin-right: 20px;">' +
+                '<a href="#" class="link icon-only open-panel"></i></a></div></div>';
+        nb.innerHTML = cDom;        
+        navbars.append(nb);
+        for(var j = 0; j < subjectClasses[i].length; j++) {
+            var lidom = document.createElement("li");
+            lidom.className = "item-content"; 
+            var dDom = '<a href="subject'+i+String.fromCharCode(65+i) +
+                        '" class="item-link"> <div class="item-content"> <div class="item-inner">' +
+                        '<div class="item-title">' + subjectClasses[i][j] + '</div></div></div></a>';
+            var ulname = ".ulsubject" + i;
+            var ul = $$(ulname);
+            lidom.innerHTML = dDom;
+            ul.append(lidom);
+        }
+    }
 }
