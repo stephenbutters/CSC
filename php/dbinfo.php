50925e727480269178c74cd3c37cb9eb57933adb
stephenbutters <stephenbutters@users.noreply.github.com>
2017-11-21 17:50:54 -0800

    Add files via upload

---
+++ php/dbinfo.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_SERVER', 'realone.c0hpz27iuq3x.us-west-1.rds.amazonaws.com');
+define('DB_USERNAME', 'hongkan');
+define('DB_PASSWORD', 'aa6418463');
+define('DB_DATABASE', 'realone');
+?>
