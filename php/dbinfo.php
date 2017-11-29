9ab2de755ee7a06b5b66a39d83b2adcfccf6bca1
stephenbutters <stephenbutters@users.noreply.github.com>
2017-11-28 18:10:38 -0800

    Delete dbinfo.php

--- php/dbinfo.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_SERVER', 'realone.c0hpz27iuq3x.us-west-1.rds.amazonaws.com');
-define('DB_USERNAME', 'hongkan');
-define('DB_PASSWORD', 'aa6418463');
-define('DB_DATABASE', 'realone');
-?>
